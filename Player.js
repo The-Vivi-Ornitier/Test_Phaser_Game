eb8f7726709f3dc7c7ede3e79e8d3cac23d27392
The-Vivi-Ornitier <thisissomuchtrash@gmail.com>
2022-08-19 02:44:10 -0400

    Cleanup up, comment, and split code as necessary, started on implementing upgrades

---
+++ Player.js
@@ -0,0 +1,49 @@
+import eventsCenter from "./EventsCenter.js";
+
+export default class Player extends Phaser.Physics.Arcade.Sprite{
+
+    constructor(scene, x, y){
+
+        super(scene, x, y, 'vivi'); // The frame is optional 
+        //this= this.physics.add.sprite(840, 525, 'vivi');
+        //this.setCollideWorldBounds(true);
+        scene.physics.world.enable(this);
+        this.scene.add.existing(this);
+
+        
+        this.body.maxVelocity.x = 400;
+        this.body.maxVelocity.y = 400; //acceleration and drag logic, not a fan, might change
+        this.body.setDamping(true);
+        this.body.setDrag(.01);
+        this.accel = 1200;
+        this.xMove = 0;
+        this.yMove = 0;
+    }
+
+    update(keys){
+
+
+
+        //update movement
+        if(keys.left.isDown && !keys.right.isDown){
+            this.xMove = -1;
+        }
+        else if (keys.right.isDown ){
+            this.xMove = 1;
+        }
+        else{
+            this.xMove = 0;
+        }
+        if(keys.up.isDown && !keys.down.isDown){
+            this.yMove = -1;
+        }
+        else if (keys.down.isDown ){
+            this.yMove = 1;
+        }
+        else{
+            this.yMove = 0;
+        }
+
+        this.setAcceleration(this.accel*this.xMove, this.accel*this.yMove);
+    }
+}
--- scenes/SceneUpgrade.js
+++ scenes/SceneUpgrade.js
@@ -1,4 +1,5 @@
 import eventsCenter from "../EventsCenter.js";
+import UpgradeCards from "../UpgradeCards.js";
 
 export default class SceneUpgrade extends Phaser.Scene {
 
@@ -7,29 +8,37 @@ export default class SceneUpgrade extends Phaser.Scene {
     }
 
     init(){
-        this.backText = this.add.text(840,525, 'UPGRADE GET');
+        
+        this.backText = this.add.text(840,1000, 'UPGRADE GET');
+        this.upgrades;
     }
 
     create() {
         this.scene.setVisible(false);
+        this.upgrades = this.add.group();
         eventsCenter.on('upgradeGet', this.upgradeGet, this);
-        //this.backText.setInteractive({ useHandCursor: true });
         this.backText.on('pointerdown', () => this.resumeButton());
     }
  
+    //test button to continue without an upgrade
+    //could also just call after getting it
     resumeButton() {
-         //this.scene.resume('SceneMain');
-         //this.scene.remove('ScenePause');
-         this.scene.setVisible(false);
+         this.scene.setVisible(false);//prob better way to do this
          this.backText.disableInteractive();
-         //this.backText.setInteractive({ useHandCursor: false });
+         this.upgrades.clear(true,true);//empty group and delete the old graphics
          eventsCenter.emit('upgradeComplete');
-         //this.scene.run('SceneMain');
     }
 
+    //initialize upgrade choices on level
     upgradeGet(){
         //backText.setColor(0x000);
         this.scene.setVisible(true);
+        this.upgrade1 = new UpgradeCards(this, 150, 100);
+        this.upgrade2 = new UpgradeCards(this, 650, 100);
+        this.upgrade3 = new UpgradeCards(this, 1150, 100);
+        this.upgrades.add(this.upgrade1);
+        this.upgrades.add(this.upgrade2);
+        this.upgrades.add(this.upgrade3);
         this.scene.bringToTop('SceneUpgrade');
         this.backText.setInteractive({ useHandCursor: true });
         //this.scene.setInteractive();
